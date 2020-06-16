11e849eb780346196d6867f812996b489479038e
Steve Seguin <steve@seguin.email>
2020-06-16 03:26:20 -0400

    Add files via upload

--- chromicam.py
+++ chromicam.py
@@ -1,66 +1,69 @@
+## Install:https://github.com/CatxFish/obs-virtual-cam/releases
+### THEN run the following, if OBS is not installed.
+# regsvr32 /n /i:1 "obs-virtualcam\bin\32bit\obs-virtualsource.dll"
+# regsvr32 /n /i:1 "obs-virtualcam\bin\64bit\obs-virtualsource.dll"
+### AND FINALLY..
+## pip install -r requirements.txt
+# python3 chromicam.py https://obs.ninja/?view=xxxxx 1280 720
+
 from cefpython3 import cefpython as cef
 import os
-import platform
-import subprocess
 import sys
 import pyvirtualcam
 import numpy as np
 import time
 import threading
 
-try:
-    from PIL import Image, PILLOW_VERSION
-except ImportError:
-    print("Error: PIL module not available. To install"
-          " type: pip install Pillow")
-    sys.exit(1)
     
 # Config
 URL = "https://www.youtube.com/watch?v=oHg5SJYRHA0"
 VIEWPORT_SIZE = (1280, 720)
+FRAMERATE = 30
 
 def command_line_arguments():
-    if len(sys.argv) == 4:
+    if len(sys.argv) > 1:
         url = sys.argv[1]
-        width = int(sys.argv[2])
-        height = int(sys.argv[3])
         if url.startswith("http://") or url.startswith("https://"):
             global URL
             URL = url
         else:
             print("Error: Invalid url argument")
             sys.exit(1)
+    if len(sys.argv) > 3:
+        width = int(sys.argv[2])
+        height = int(sys.argv[3])
         if width > 0 and height > 0:
             global VIEWPORT_SIZE
             VIEWPORT_SIZE = (width, height)
         else:
             print("Error: Invalid width and height")
             sys.exit(1)
-
-    elif len(sys.argv) > 1:
-        print("Error: Expected arguments: url width height")
-        sys.exit(1)
+    if len(sys.argv) > 4:
+        global FRAMERATE
+        FRAMERATE = int(sys.argv[4])
+        
    
 
 class LoadHandler(object):
     def OnLoadingStateChange(self, browser, is_loading, **_):
         pass
 
-def drawFrame(browser, run_event):
+def drawFrame(browser, run_event, camera_ready):
     time.sleep(2)
-    with pyvirtualcam.Camera(width=1280, height=720, fps=20, print_fps=True) as cam:
-        print("start camara")
+    with pyvirtualcam.Camera(width=VIEWPORT_SIZE[0], height=VIEWPORT_SIZE[1], delay=0, fps=20, print_fps=True) as cam:
+        print("Initializing OBS-VirtualCam..")
         while run_event.is_set():
+            camera_ready.wait()
             buffer_string = browser.GetUserData("OnPaint.buffer_string")
             if (buffer_string):
-                image = Image.frombytes("RGBA", VIEWPORT_SIZE, buffer_string, "raw", "RGBA", 0, 1)
-                image = np.array(image).astype("uint8")
-                cam.send(image)
-                cam.sleep_until_next_frame()
+                cam.send(np.frombuffer(buffer_string, dtype=np.uint8, count=(VIEWPORT_SIZE[0]*VIEWPORT_SIZE[1]*4)).reshape((VIEWPORT_SIZE[0],VIEWPORT_SIZE[1],4)))
+                
+                camera_ready.clear()
+                #cam.sleep_until_next_frame()
 
 class RenderHandler(object):
     def __init__(self):
-        print('start')
+        print('Starting to Render Frames')
         pass
 
     def GetViewRect(self, rect_out, **_):
@@ -68,15 +71,19 @@ class RenderHandler(object):
         return True
 
     def OnPaint(self, browser, element_type, paint_buffer, **_):
+        global camera_ready
         if element_type == cef.PET_VIEW:
             buffer_string = paint_buffer.GetString(mode="rgba", origin="top-left")
             browser.SetUserData("OnPaint.buffer_string", buffer_string)
+            camera_ready.set();
         
-global t,run_event
+global t,run_event, camera_ready
+
 def endProgram(exctype, value, tb):
-    global t,run_event
+    global t, run_event, camera_ready
     print("EXITING");
     run_event.clear()
+    camera_ready.set()
     t.join()
     cef.QuitMessageLoop()
     browser.CloseBrowser()
@@ -110,6 +117,9 @@ try:
     window_info = cef.WindowInfo()
     window_info.SetAsOffscreen(parent_window_handle)
     
+    camera_ready = threading.Event()
+    camera_ready.set()
+    
     browser = cef.CreateBrowserSync(window_info=window_info, settings=browser_settings, url=URL)
     browser.SetClientHandler(RenderHandler())
     
@@ -119,11 +129,13 @@ try:
     
     run_event = threading.Event()
     run_event.set()
-    t = threading.Thread(target=drawFrame, args=(browser,run_event,))
+    
+    t = threading.Thread(target=drawFrame, args=(browser,run_event,camera_ready,))
     t.start()
     
     cef.MessageLoop()
     run_event.clear()
+    camera_ready.set()
     t.join()
     
     cef.QuitMessageLoop()
---
+++ readme.md
@@ -0,0 +1,14 @@
+You'll need to install: https://obsproject.com/forum/resources/obs-virtualcam.949/
+
+Stop OBS Virtual Cam before using with this tool if its already in use. 
+(Stop it in OBS if using it there, for example)
+
+usage:
+chromicam https://obs.ninja/?view=xxx 1280 720 30
+
+or just chromicam.exe to test.
+
+If it crashes, deactive and reaactivate OBS Virtualcam (via its OBS device settings works)
+Also double check to make sure its not already in use by OBS or other app.
+
+This app will use the first OBS Virtualcam; others install are ignored.
